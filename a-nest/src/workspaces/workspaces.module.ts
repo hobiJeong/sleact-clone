459e7c19ddc01943692c8fe90f1ffa58bbac3622
hobiJeong <jjb26433@naver.com>
2023-09-06 17:37:12 +0900

    feat: 진행상황 push

--- a-nest/src/workspaces/workspaces.module.ts
+++ a-nest/src/workspaces/workspaces.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { WorkspacesController } from './workspaces.controller';
 
-@Module({})
+@Module({
+  controllers: [WorkspacesController]
+})
 export class WorkspacesModule {}
